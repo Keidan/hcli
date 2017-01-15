9795271856bcfb65d928ad55aa8819c51b2a879c
Keidan (K. Billonneau) <kuso.fr@gmail.com>
2017-01-15 16:23:27 +0100

    add class to manage the HTTP headers/cookies

---
+++ src/HttpHeader.cpp
@@ -0,0 +1,190 @@
+/**
+*******************************************************************************
+* <p><b>Project hcli</b><br/>
+* </p>
+* @author Keidan
+*
+*******************************************************************************
+*/
+#include "HttpHeader.hpp"
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+
+namespace net {
+  namespace http {
+
+    using std::istringstream;
+    using std::vector;
+    using std::string;
+    using std::size_t;
+    using std::istream_iterator;
+    using helper::vstring;
+    using helper::Helper;
+
+    HttpHeader::HttpHeader() : _done(false), _code(0), _reason(""), _content(), _hdrs(), _length(0) {
+    }
+
+    HttpHeader::~HttpHeader() {
+      clear();
+    }
+
+    /**
+     * @brief Clear the headers contexts.
+     */
+    auto HttpHeader::clear() -> void {
+      _done = false;
+      _code = 0;
+      _length = 0;
+      _reason = "";
+      _content.str("");
+      _hdrs.clear();
+    }
+
+    /**
+     * @brief Test if all the headers are sent.
+     * @return true if done.
+     */
+    auto HttpHeader::done() -> bool {
+      return _done;
+    }
+
+    /**
+     * @biref Get the response code.
+     * @return The code.
+     */
+    auto HttpHeader::code() -> unsigned int {
+      return _code;
+    }
+
+    /**
+     * @brief Get the header length.
+     * @return length
+     */
+    auto HttpHeader::length() -> std::size_t {
+      return _length;
+    }
+
+    /**
+     * @biref Get the response reason.
+     * @return The reason.
+     */
+    auto HttpHeader::reason() -> string {
+      return _reason;
+    }
+
+    /**
+     * @brief Append data to decode.
+     * @param data The data.
+     */
+    auto HttpHeader::append(const string& data) -> void {
+      if(!_done) {
+	_content << data;
+	string s = _content.str();
+	size_t found = s.find("\r\n\r\n");
+	/* get all the headers */
+	if (found!=string::npos) {
+	  _done = true;
+	  /* only preserve the headers */
+	  s = s.substr(0, found);
+	  _length = s.length() + 4;
+	  /* prepare for header parsing */
+	  vstring tokens = Helper::split(s, '\n');
+	  for(vstring::const_iterator it = tokens.begin(); it != tokens.end(); ++it) {
+	    string line = *it;
+	    /* extract the code and the reason */
+	    if(_code == 0 && (found = line.find("HTTP/")) != string::npos) {
+	      line = line.substr(found + 9);
+	      found = line.find(" ");
+	      if(found != string::npos) {
+		_code = std::atoi(line.substr(0, found).c_str());
+		_reason = Helper::trim(Helper::trim(line.substr(found+1)), '\r');
+	      }
+	      /* now extract the headers */
+	    } else if(_code != 0) {
+	      found = line.find(":");
+	      if(found != string::npos) {
+		string key = line.substr(0, found);
+		string val = Helper::trim(Helper::trim(line.substr(found+1)), '\r');
+		auto it = find(key);
+		if(it != _hdrs.end()) {
+		  //std::cout << "Update key '" << key << "' with value '" << val << "'" << std::endl;
+		  it->second.push_back(val);
+		}
+		else {
+		  vstring vs;
+		  vs.push_back(val);
+		  //std::cout << "Add key '" << key << "' with value '" << val << "'" << std::endl;
+		  _hdrs.push_back(std::make_pair(key, vs));
+		}
+	      }
+	    }
+	  }
+	}
+      }
+    }
+      
+
+    /**
+     * @brief Find an entry from the key.
+     * @param key The key to find.
+     * @return The value.
+     */
+    auto HttpHeader::find(const string& key) -> HttpHeadersIterator {
+      auto it = std::find_if(_hdrs.begin(), _hdrs.end(),
+			     [&key](const HttpHeaderItem& element){ return element.first == key;} );
+      return it;
+    }
+
+    /**
+     * @brief Test if the key is present or not.
+     * @param key The header key.
+     * @return true if the key is present.
+     */
+    auto HttpHeader::contains(const string& key) -> bool {
+      return find(key) != _hdrs.end();
+    }
+
+    /**
+     * @brief Test if the key is present and if the value match.
+     * @param key The header key.
+     * @param value The header value.
+     * @return true if the key is present and match.
+     */
+    auto HttpHeader::equals(const std::string& key, const std::string& value) -> bool {
+      auto it = find(key);
+      if(it != _hdrs.end()) {
+	if(std::find(it->second.begin(), it->second.end(), value) != it->second.end())
+	  return true;
+      }
+      return false;
+    }
+
+    /**
+     * @brief Get the keys list.
+     * @return The list of keys.
+     */
+    auto HttpHeader::keys() -> vstring {
+      vstring v;
+      for(HttpHeadersConstIterator it = _hdrs.begin(); it != _hdrs.end(); ++it) {
+	string i = it->first;
+	v.push_back(i);
+      }
+      return v;
+    }
+
+    /**
+     * @brief Get the specific header value.
+     * @param key The header key.
+     * @return The value.
+     */
+    auto HttpHeader::get(const string& key) -> vstring {
+      vstring v;
+      auto it = find(key);
+      if(it != _hdrs.end())
+	v = it->second;
+      return v;
+    }
+
+  } /* namespace http */
+} /* namespace net */
---
+++ src/HttpHeader.hpp
@@ -0,0 +1,111 @@
+/**
+*******************************************************************************
+* <p><b>Project hcli</b><br/>
+* </p>
+* @author Keidan
+*
+*******************************************************************************
+*/
+#ifndef __HTTPHEADER_H__
+#define __HTTPHEADER_H__
+
+#include "Helper.hpp"
+#include <utility>
+
+namespace net {
+  namespace http {
+
+    class HttpHeader {
+      public:
+
+	HttpHeader();
+	~HttpHeader();
+
+	using HttpHeaderItem = std::pair<std::string, helper::vstring>;
+	using HttpHeaders = std::vector<HttpHeaderItem>;
+	using HttpHeadersIterator = HttpHeaders::iterator;
+	using HttpHeadersConstIterator = HttpHeaders::const_iterator;
+      
+	/**
+	 * @brief Clear the headers contexts.
+	 */
+	auto clear() -> void;
+
+	/**
+	 * @brief Test if all the headers are sent.
+	 * @return true if done.
+	 */
+	auto done() -> bool;
+
+	/**
+	 * @brief Append data to decode.
+	 * @param data The data.
+	 */
+	auto append(const std::string& data) -> void;
+      
+	/**
+	 * @brief Get the specific header value.
+	 * @param key The header key.
+	 * @return The value.
+	 */
+	auto get(const std::string& key) -> helper::vstring;
+
+	/**
+	 * @brief Test if the key is present or not.
+	 * @param key The header key.
+	 * @return true if the key is present.
+	 */
+	auto contains(const std::string& key) -> bool;
+
+	/**
+	 * @brief Test if the key is present and if the value match.
+	 * @param key The header key.
+	 * @param value The header value.
+	 * @return true if the key is present and match.
+	 */
+	auto equals(const std::string& key, const std::string& value) -> bool;
+
+	/**
+	 * @brief Get the keys list.
+	 * @return The list of keys.
+	 */
+	auto keys() -> helper::vstring;
+
+	/**
+	 * @biref Get the response code.
+	 * @return The code.
+	 */
+	auto code() -> unsigned int;
+
+	/**
+	 * @biref Get the response reason.
+	 * @return The reason.
+	 */
+	auto reason() -> std::string;
+
+	/**
+	 * @brief Get the header length.
+	 * @return length
+	 */
+	auto length() -> std::size_t;
+
+
+      private:
+	bool _done;
+	unsigned int _code;
+	std::string _reason;
+	std::stringstream _content;
+	HttpHeaders _hdrs;
+	std::size_t _length;
+
+	/**
+	 * @brief Find an entry from the key.
+	 * @param key The key to find.
+	 * @return The value.
+	 */
+	auto find(const std::string& key) -> HttpHeadersIterator;
+    };
+
+  } /* namespace http */
+} /* namespace net */
+#endif /* __HTTPHEADER_H__ */
